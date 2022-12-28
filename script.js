e8d1fde4679a1a0a7c00de00cfed270e6ab92d64
Aleksandra Sanecka <a.saneckaa@gmail.com>
2022-12-28 13:51:39 +0100

    binding events moved to a separate function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 {
-    const tasks = [];
-
+    const tasks =[];
     
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -9,30 +8,59 @@
         render();
     };
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done=!tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(taskIndex);
+            });
+        });
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li class="list__item${task.done ? " list__item--done" : ""}">
+            <button class="task__button--toggle js-done">Done?</button>
             ${task.content}
+            <button class="task__button--remove list__button js-remove">Remove</button>
             </li>
             `;
         };
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
+        bindEvents();
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-        if(newTaskContent === "") {
+        if (newTaskContent === "") {
             return;
         };
 
-       addNewTask(newTaskContent);
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
@@ -41,6 +69,6 @@
         form.addEventListener("submit", onFormSubmit)
         render();
     };
-    
+
     init();
 }
